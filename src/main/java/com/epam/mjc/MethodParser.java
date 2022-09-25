3f2deb7c38c0ab6a3e05f2f81bfcedc28d0fe2cc
Alex-Kross <artemkirlanovia@gmail.com>
2022-09-26 00:00:51 +0300

    task completed

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,7 +1,12 @@
 package com.epam.mjc;
 
-public class MethodParser {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MethodParser {
+    private static final String splitterArgAndBeforeArg = "[\\(\\)]";
+    private static final String splitterBetweenWords = " ";
+    private static final String splitterBetweenArgs = ", ";
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
@@ -20,6 +25,47 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringSplitter splitter = new StringSplitter();
+        ArrayList<String> mainDelimiter = new ArrayList<>();
+        mainDelimiter.add(splitterArgAndBeforeArg);
+        //split arguments and before arguments
+        ArrayList<String> listArgAndBeforeArg = (ArrayList<String>) splitter.splitByDelimiters(signatureString, mainDelimiter);
+        //if method have args
+        List<MethodSignature.Argument> arguments= new ArrayList<>();
+        if (listArgAndBeforeArg.size() == 2) {
+            String argumentsMethod = listArgAndBeforeArg.get(1);
+            ArrayList<String> argsDelimiter = new ArrayList<>();
+            argsDelimiter.add(splitterBetweenArgs);
+            argsDelimiter.add(splitterBetweenWords);
+            //split args on words
+            ArrayList<String> listArgs = (ArrayList<String>) splitter.splitByDelimiters(argumentsMethod, argsDelimiter);
+            for (int i = 0; i < listArgs.size(); i += 2) {
+                arguments.add(new MethodSignature.Argument(listArgs.get(i), listArgs.get(i + 1)));
+            }
+        }
+        //split access modifier, return type and name
+        String preArgumentMethod = listArgAndBeforeArg.get(0);
+        ArrayList<String> beforeArgsDelimiter = new ArrayList<>();
+        beforeArgsDelimiter.add(splitterBetweenWords);
+        ArrayList<String> listBeforeArgs = (ArrayList<String>) splitter.splitByDelimiters(preArgumentMethod, beforeArgsDelimiter);
+
+        String methodRetType;
+        String methodName;
+        MethodSignature methodSignature;
+        //if method have access modifier
+        if (listBeforeArgs.size() == 3) {
+            String methodModAcc = listBeforeArgs.get(0);
+            methodRetType = listBeforeArgs.get(1);
+            methodName = listBeforeArgs.get(2);
+            methodSignature = new MethodSignature(methodName, arguments);
+            methodSignature.setAccessModifier(methodModAcc);
+
+        } else {
+            methodRetType = listBeforeArgs.get(0);
+            methodName = listBeforeArgs.get(1);
+            methodSignature = new MethodSignature(methodName, arguments);
+        }
+        methodSignature.setReturnType(methodRetType);
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,32 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
      * @param source source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        ArrayList<String> listElOfSource = new ArrayList<>();
+        listElOfSource.add(source);
+        for (String delimiter : delimiters) {
+            ArrayList<String> newListElOfSource = new ArrayList<>();
+            for (String element : listElOfSource){
+                String[] split = element.split(delimiter);
+                Collections.addAll(newListElOfSource, split);
+            }
+            listElOfSource = newListElOfSource;
+        }
+        //delete empty elements
+        while(listElOfSource.remove(""));
+        return listElOfSource;
     }
 }
